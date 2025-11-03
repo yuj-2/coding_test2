1ddec02469256deb2e909782c20dbc97e19357fe
yuj-2 <138671580+yuj-2@users.noreply.github.com>
2025-11-03 15:09:31 +0900

    [level 0] Title: 최댓값 만들기 (1), Time: 0.54 ms, Memory: 72.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/120847. 최댓값 만들기 （1）/최댓값 만들기 （1）.java
@@ -0,0 +1,8 @@
+import java.util.Arrays;
+class Solution {
+    public int solution(int[] numbers) {
+        Arrays.sort(numbers);
+        int len = numbers.length;
+        return numbers[len-2] * numbers[len-1];
+    }
+}
